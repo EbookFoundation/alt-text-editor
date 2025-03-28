b7588822acc606fa418c0632eaa2f9486ed0f48d
Rowan McKereghan <rowanmckereghan@gmail.com>
2025-03-27 20:08:41 -0700

    votes, sources, preferred visually distinct

--- alt-text-react-app/src/AltTexts.jsx
+++ alt-text-react-app/src/AltTexts.jsx
@@ -1,22 +1,53 @@
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
+import Votes from './Votes';
+import './alt.css'
 
 
 export default function AltTexts({stateObj}) {
 
-    const mappedAlts = (alt_text, alt_key, index) => {
+    //return img obj instead of alt obj when sending 201? up for discussion
+        //see update button comment (two users updating same book)
+
+    //display source name to the right of alt text (agent)
+        //name of source but no user - special case
+    //instead of radio buttons, reddit upvote/downvote system
+        //keep track in "status" key in table, sort by status rating, hide negative status objs until user requests to see
+        //when users start being added, endorsement table to keep track (backend)
+            //keep in mind that status is not displayed yet, be ready to refactor to display status
+        //add ai suggestion to this reddit post style list
+    //make preferred alt text display visually distinct (border, color, etc.)
+    //save user input alt text locally with save button, make it stateful on reload
+        //start on image that we left off on
+        //for later: alert popup on reload to save before reloading
+    //update button: pull img obj selected, update alt text list
+
+    //presentational elements:
+        //have some feature to be able to mark as presentational, mark as one of two options as radio list
+        //"This image is adequately described by nearby text."
+        //"This is a presentational image... blah blah"
+        //do not display or have any editable alt text on these <imgs>
+        //first as button, then as checkdown list once above gets working adequately
+            //checkdown list with user display (alt text edited, unedited, presentational, no objs yet, submitted, etc.)
+
+    //img "type" field:
+        //0: normal
+        //1: purely decorational
+        //2: cover
+        //3: button
+        //-1: other?
+
+    const mappedAlts = (alt_text, img_key, alt_key, source, index) => {
         if(alt_text === "") {return;}
         return (
             <InputGroup key={alt_key}>
-                <InputGroup.Radio name="altTextSubmission" onChange={(e) => stateObj["userSubRadioValue"][1](e.currentTarget.value)}/>
-                <FloatingLabel label={"Alt Text Option " + (index + 1)} controlId={'altText_' + alt_key}>
+                <Votes vote_identifier={"img_" + img_key + "_alt_" + alt_key}></Votes>
+                <FloatingLabel label={"Option " + (index + 1) + " (source: " + source + ")"} controlId={'altText_' + alt_key}>
                     <Form.Control disabled as='textarea' style={{"height": "100px"}} value={alt_text}></Form.Control>
                 </FloatingLabel>
             </InputGroup>
         );
-
-        
     }
 
     const imgAltObj = stateObj["imgIdtoAltsMap"][0][stateObj["imgToggleValue"][0]];
@@ -27,15 +58,21 @@ export default function AltTexts({stateObj}) {
         );
     }
 
+    const pref = imgAltObj.preferred_alt_text;
+
+    function PreferredVotes() {
+        return (<Votes vote_identifier={"img_" + pref.img + "_alt_" + pref.id}></Votes>);
+    }
+
     return (
         <>
             <InputGroup>
-                <InputGroup.Radio name="altTextSubmission" onChange={(e) => stateObj["userSubRadioValue"][1](e.currentTarget.value)}/>
-                <FloatingLabel label="Current Preferred Alt Text" controlId='altTextPref'>
-                    <Form.Control disabled as='textarea' style={{"height": "100px"}} value={imgAltObj.preferred_alt_text.text}></Form.Control>
+                <PreferredVotes/>
+                <FloatingLabel label={"Preferred (source: " + pref.source + ")"} controlId='altTextPref' className='preferred'>
+                    <Form.Control disabled as='textarea' style={{"height": "100px"}} value={pref.text}></Form.Control>
                 </FloatingLabel>
             </InputGroup>
-            {imgAltObj.alts_arr.map((altObj, index) => mappedAlts(altObj.text, altObj.id, index))}
+            {imgAltObj.alts_arr.map((altObj, index) => mappedAlts(altObj.text, altObj.img, altObj.id, altObj.source, index))}
         </> 
     );
 
---
+++ alt-text-react-app/src/Votes.jsx
@@ -0,0 +1,57 @@
+import './alt.css';
+import { useState, useRef } from 'react';
+
+
+export default function Votes({vote_identifier}) {
+    const upvoteRef = useRef(null);
+    const downvoteRef = useRef(null);
+
+    const [upvoteChecked, setUpvoteChecked] = useState(false);
+    const [downvoteChecked, setDownvoteChecked] = useState(false);
+
+    const upvoteToggle = () => {
+        if(downvoteRef.current.checked) {
+            downvoteRef.current.checked = false;
+            setDownvoteChecked(false);
+        }
+
+        setUpvoteChecked(upvoteRef.current.checked);
+    }
+
+    const downvoteToggle = () => {
+        if(upvoteRef.current.checked) {
+            upvoteRef.current.checked = false;
+            setUpvoteChecked(false);
+        }
+
+        setDownvoteChecked(downvoteRef.current.checked);
+    }
+
+    const upvoteStyle = {
+        "backgroundColor": upvoteChecked ? "#59a9f9" : "inherit",
+    }
+
+    const downvoteStyle = {
+        "backgroundColor": downvoteChecked ? "#F88107" : "inherit",
+    }
+
+
+    return (
+        <span className="input-group-text">
+            <input className="form-check-input input-upvote" type="checkbox" name="altTextSubmission" 
+            id={vote_identifier + "_up"} ref={upvoteRef} onChange={upvoteToggle}/>
+            <input className="form-check-input input-downvote" type="checkbox" name="altTextSubmission" 
+            id={vote_identifier + "_down"} ref={downvoteRef} onChange={downvoteToggle}/>
+            <label htmlFor={vote_identifier + "_up"}>
+                <svg style={upvoteStyle} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="vote bi bi-arrow-up-square" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
+                </svg>
+            </label>
+            <label htmlFor={vote_identifier + "_down"}>
+                <svg style={downvoteStyle} xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="16" height="16" className="vote bi bi-arrow-down-square" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
+                </svg>
+            </label>
+        </span>
+    );
+}
